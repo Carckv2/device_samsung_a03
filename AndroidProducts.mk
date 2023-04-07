46eb9c08b4962921ed82530160ada1488988e1c8
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-04-07 11:34:35 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a03.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_a03-user \
-    lineage_a03-userdebug \
-    lineage_a03-eng
+    $(LOCAL_DIR)/aosp_a03.mk
