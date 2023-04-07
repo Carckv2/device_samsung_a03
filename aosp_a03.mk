c6c94acd0be5c68d7874a1973939df20f18a3418
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-04-07 11:34:08 -0400

    Update and rename lineage_a03.mk to aosp_a03.mk

--- aosp_a03.mk
+++ aosp_a03.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from a03 device
 $(call inherit-product, device/samsung/a03/device.mk)
 
 PRODUCT_DEVICE := a03
-PRODUCT_NAME := lineage_a03
+PRODUCT_NAME := aosp_a03
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A035F
 PRODUCT_MANUFACTURER := samsung
